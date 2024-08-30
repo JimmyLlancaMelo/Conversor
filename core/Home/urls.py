c9be01453feb62744a22d10ed6a086322515a857
JimmyLlancaMelo <windowsrevolutions501@gmail.com>
2024-08-30 12:34:10 -0500

    Construyendo la interfaz

--- core/Home/urls.py
+++ core/Home/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import Home, Download
+from .views import Home
 
 app_name = "home"
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('download/', Download.as_view(), name='download')
 ]
--- core/Home/views.py
+++ core/Home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .toolConverter import obtener_enlaces_descarga
 
 # Create your views here.
 
@@ -10,17 +9,4 @@ class Home(View):
         return render(request, 'home.html')
 
     def post(self, request):
-        video_url = request.POST.get('video_url')
-        
-        if video_url:
-            resultados = obtener_enlaces_descarga(video_url)
-            return render(request, 'download.html', {
-                'enlaces_mp4': resultados['mp4'],
-                'enlaces_mp3': resultados['mp3'],
-            })
-        else:
-            return render(request, 'home.html', {'error': 'Por favor, ingrese una URL válida.'})
-
-class Download(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'download.html')
+        pass
