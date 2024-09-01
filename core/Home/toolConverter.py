deb24dbe6f4877596c152c8beb2c7ec548bf473d
JimmyLlancaMelo <windowsrevolutions501@gmail.com>
2024-08-31 21:07:56 -0500

    mejorando la logica

---
+++ core/Home/toolConverter.py
@@ -0,0 +1,70 @@
+import yt_dlp
+
+class toolVideo:
+    
+    def __init__(self, url):
+        self.url = url
+        self.listaVideo = []
+        self.listaAudio = []
+
+    def Info(self):
+        with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
+            info = ydl.extract_info(self.url, download=False) # SE OBTIENE LA INFORMACION EN UN DICCIONARIO
+
+            for i in info['formats']: # SE OBTIENEN VARIOS FORMATOS - formad_id, url, ext, acodec, vcodec, filesize, abr, etc
+
+                self.listaVideo.append(i.get("height","desconocido")) # OPTENER CALIDAD DE 1080p 720p 480p 360p 240p
+                self.listaAudio.append(i.get("abr","desconocido")) # OBTENER CALIDAD DE 320kbs 192kbs 128kbs
+
+            # QUITAR DUPLICADOS
+            self.listaVideo = list(set(self.listaVideo))
+            self.listaAudio = list(set(self.listaAudio))
+
+            # DEJAR NUMEROS Y DECIMALES
+            self.listaVideo = list(i for i in self.listaVideo if isinstance(i,int))
+            self.listaAudio = list(i for i in self.listaAudio if isinstance(i,float))
+            
+            # ORDENAR
+            self.listaVideo.sort(reverse=True) # DE MAYOR A MENOR
+            self.listaAudio.sort(reverse=True)
+            
+            # FILTRAR
+            self.listaVideo = [i for i in self.listaVideo if i > 100] # ELIMINAMOS ELEMENTOS MENORES A 100
+            del self.listaAudio[3:len(self.listaAudio)] # ELIMINAMOS ELEMENTOS A PARTIR DEL INDICE 3 EN ADELANTE
+            self.listaAudio[:3] = [320,192,128] # REEMPLAZAMOS LOS ELEMENTOS
+            
+            print(self.listaVideo)
+            print(self.listaAudio)
+
+    def download(self, choice1, choice2,choice3):
+        print(self.url)
+        print(self.listaVideo)
+        if choice1 == 1:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': f'{self.listaAudio[choice2]}',
+                }],
+                'extractaudio': True,
+                'outtmpl': '%(title)s.%(ext)s',
+            }
+            
+        else:
+            ydl_opts = {
+                'format': f'bestvideo[height<={self.listaVideo[choice3]}]+bestaudio/best',
+                'merge_output_format': 'mp4',
+                'outtmpl': '%(title)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegVideoConvertor',
+                    'preferedformat': 'mp4',
+                }],
+                'postprocessor_args': [
+                    '-c:v', 'copy',
+                    '-c:a', 'aac',
+                    '-b:a', '320k'
+                ],
+            }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.url])
--- core/Home/views.py
+++ core/Home/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from .toolConverter import toolVideo
 
 # Create your views here.
+
+URLYOUTUBE = []
 
 class Home(View):
     
     def get(self, request):
+        # Renderiza el template 'home.html' para solicitudes GET
         return render(request, 'home.html')
 
     def post(self, request):
-        pass
+        URLYOUTUBE.append(request.POST.get('urlYoutube', ''))
+        print(URLYOUTUBE)
+        
+        action = request.POST.get('action', '')
+        
+        if action == 'form1':
+            urlVideo = toolVideo(URLYOUTUBE[0])
+
+            urlVideo.Info()
+            context = {
+                'calidad': urlVideo.listaAudio
+            }
+            return render(request, 'home.html', context)
+
+        elif action == 'form2':
+            print(URLYOUTUBE)
+            urlVideo = toolVideo(URLYOUTUBE[0])
+            urlVideo.Info()
+            urlVideo.download(1,urlVideo.listaAudio[0],2)
+        
+        return redirect('home:home')
